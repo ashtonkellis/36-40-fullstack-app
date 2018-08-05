e0575c828cb6eca395be9d03b146032c6e8651eb
Ashton K Ellis <ashtonkellis@gmail.com>
2018-08-04 23:32:49 -0700

    files uploading to amazon s3 bucket

--- src/components/file-form/file-form.js
+++ src/components/file-form/file-form.js
@@ -3,29 +3,22 @@ import PropTypes from 'prop-types';
 import './file-form.scss';
 
 const fileToBase64String = (file) => {
-  // because FileReader reads contents asynchronously, we wrap it in a promise to handle the data flow
   return new Promise((resolve, reject) => {
     if (!file) {
       return reject(new Error('File Required'));
     }
 
-    // The FileReader object lets web applications asynchronously read the contents of files (or raw data buffers) stored on the user's computer, using File or Blob objects to specify the file or data to read.
-    // instantiate a new filereader
     const fileReader = new FileReader();
 
-    // The FileReader.onload property contains an event handler executed when the load event is fired, when content read with readAsArrayBuffer, readAsBinaryString, readAsDataURL or readAsText is available.
-    fileReader.addEventListener('load', () => resolve(fileReader.result)); // .result is the file's base 64 representation 
+    fileReader.addEventListener('load', () => resolve(fileReader.result));
     fileReader.addEventListener('error', reject);
-    // The readAsDataURL method is used to read the contents of the specified Blob or File. When the read operation is finished, the readyState becomes DONE, and the loadend is triggered. At that time, the result attribute contains  the data as a data: URL representing the file's data as a base64 encoded string.
     fileReader.readAsDataURL(file);
     return undefined;
   });
 };
 
 const stopEvents = (event) => {
-  // this isn't really that necessary because our dragging events are on the top level window, but still good to know
   event.stopPropagation();
-  // event.preventDefault is needed to stop the browser from traveling to the file itself
   event.preventDefault();
 };
 
@@ -33,9 +26,8 @@ export default class FileForm extends React.Component {
   constructor(props) {
     super(props);
 
-    // anything with "this" inside the constructor is generally available to other methods in the rest of our class (assuming arrow methods or methods that were explicitly bound)
     this.emptyState = {
-      preview: null, // base64 representation of the asset we'll upload
+      preview: null,
       file: '',
       fileType: '',
       dragOverClass: '',
@@ -70,16 +62,17 @@ export default class FileForm extends React.Component {
 
   onDrop = (event) => {
     event.preventDefault();
-    // event.dataTransfer.files is an array-like object with metadata about the files you uploaded
     const { files } = event.dataTransfer;
     const [file] = files;
+    console.log('YOU ARE HERE LINE 67');
+    console.log(files, file, 'FILES & FILE');
     fileToBase64String(file)
       .then((preview) => {
         this.setState({
           fileType: file.type,
           dragOverClass: '',
           title: file.name,
-          file: file,
+          file,
           preview,
         });
       })
@@ -87,9 +80,8 @@ export default class FileForm extends React.Component {
   }
 
   handleChange = (event) => {
-    const { type, value, files } = event.target; // notice how we now have a "files" property on event.target IF the input type is "file"
-
-    // if the input's type is file
+    const { type, value, files } = event.target;
+    console.log(type, 'TYPE');
     if (type === 'file') {
       const [file] = files;
       fileToBase64String(file)
@@ -98,7 +90,7 @@ export default class FileForm extends React.Component {
             fileType: file.type,
             dragOverClass: '',
             title: file.name,
-            file: file,
+            file,
             preview,
           });
         })
